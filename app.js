ddce84465af78acc58cbea8ce94a3684fe544722
IselaMorquecho <isela.escom@gmail.com>
2019-01-23 12:12:48 -0600

    ...

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -17,6 +16,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
