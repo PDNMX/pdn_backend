daf33fec0b60b8f89fe7788be8509dee7e1e1815
IselaMorquecho <isela.escom@gmail.com>
2019-01-24 17:28:02 -0600

    headers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-/*var bodyParser = requiere('body-parser');*/
+var bodyParser = requiere('body-parser');
 
 var fileUpload = require('express-fileupload');
 
@@ -20,9 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-/*app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({limit:'50mb', extended: true}));
-*/
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
