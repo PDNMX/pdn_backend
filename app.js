bad29f8967188c9dad4499a276c91754834f2f7e
IselaMorquecho <isela.escom@gmail.com>
2019-01-24 11:12:49 -0600

    bodyParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fileUpload = require('express-fileupload');
 var cors = require('cors');
+var bodyParser = requiere('body-parser');
 
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({limit:'50mb', extended: true}));
 app.use(fileUpload());
 app.use('/', indexRouter);
 
