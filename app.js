79211a2223da16d304d1c1cb9b76a5389d6278f8
IselaMorquecho <isela.escom@gmail.com>
2019-01-24 13:38:08 -0600

    cors config

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({limit:'50mb', extended: true}));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(fileUpload());
 app.use('/', indexRouter);
