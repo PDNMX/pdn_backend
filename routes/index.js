1d2141e81611db98476228a267b5dcc7f9d1c407
IselaMorquecho <isela.escom@gmail.com>
2019-01-24 16:50:58 -0600

    x

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var uuidv1 = require('uuid/v1');
 var router = express.Router();
+var cors = require('cors');
 
-router.post('/uploadOficio', (req, res) => {
+router.post('/uploadOficio',cors(), (req, res) => {
     let uuidSolcitud = uuidv1();
     let file = req.files.file;
     file.name=uuidSolcitud+'.pdf';
     file.mv(`./solicitudes/${file.name}`, err => {
         if (err) return res.status(500).send({status:'ERROR',message: err});
+        res.header("Access-Control-Allow-Origin", "*");
         return res.status(200).send({status: 'OK', idDocument:uuidSolcitud, nameDocument:file.name})
     })
 });
