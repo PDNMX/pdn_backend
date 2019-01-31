05a700335ffe06da7eb1cd3205c4343169799939
IselaMorquecho <isela.escom@gmail.com>
2019-01-31 13:29:33 -0600

    deleteOficio

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var uuidv1 = require('uuid/v1');
 var router = express.Router();
 var cors = require('cors');
+var fs = require('fs');
 
 router.post('/uploadOficio',cors(), (req, res) => {
     let uuidSolcitud = uuidv1();
@@ -15,4 +16,15 @@ router.post('/uploadOficio',cors(), (req, res) => {
     })
 });
 
+router.post('/deleteOficio',cors(),(req,res)=>{
+    let id = req.body.idDocument;
+    fs.exists(`./solicitudes/${id}`, function(exist){
+        if(exist){
+            fs.unlink(`./solicitudes/${id}`);
+            return res.status(200).send({status:'OK',mensaje:'Oficio '+ id + ' eliminado correctamente'})
+        }else{
+            return res.status(400).send({status:'ERROR',mensaje:'El Oficio '+ id + ' no se encuentra en el repositorio'})
+        }
+    })
+});
 module.exports = router;
