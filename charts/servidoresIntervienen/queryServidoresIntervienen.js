dade39e109d5adbe7fe8cb3bd789aa7c90c09345
IselaMorquecho <isela.escom@gmail.com>
2019-06-28 10:52:55 -0500

    querys s2

--- charts/servidoresIntervienen/queryServidoresIntervienen.js
+++ charts/servidoresIntervienen/queryServidoresIntervienen.js
@@ -65,7 +65,126 @@ router.get('/viz/servidoresIntervienen/getAgrupacionPuesto', cors(),(req,res)=>{
         })
 });
 
+router.get('/viz/servidoresIntervienen/getAgrupaciones', cors(),(req,res)=>{
+    const client = new Client(connectionData);
+    client.connect ();
+
+    client.query(
+        "select ejercicio, ramo, count(*) total  " + req.campos?req.campos : "" +
+        " from reniresp " +
+        req.filtros ? (" where "+req.filtros) : "" +
+        " group by ejercicio,ramo " + req.grupos ? req.grupos :""+
+          " order by total desc "
+    )
+        .then(response => {
+            let rows = response.rows;
+            client.end();
+            return res.status(200).send(
+                {
+                    "status": 200,
+                    "data": rows
+                });
+        })
+        .catch(err => {
+            client.end();
+            console.log("Error : ",err);
+            return res.status(400).send(
+                {
+                    "status" : 400,
+                    "mensaje" : "Error al consultar BD"
+                }
+            )
+        })
+});
+
+router.get('/viz/servidoresIntervienen/getEjercicios', cors(),(req,res)=>{
+    const client = new Client(connectionData);
+    client.connect ();
+
+    client.query(
+        "select ejercicio from reniresp group by ejercicio order by ejercicio "
+    )
+        .then(response => {
+            let rows = response.rows;
+            client.end();
+            return res.status(200).send(
+                {
+                    "status": 200,
+                    "data": rows
+                });
+        })
+        .catch(err => {
+            client.end();
+            console.log("Error : ",err);
+            return res.status(400).send(
+                {
+                    "status" : 400,
+                    "mensaje" : "Error al consultar BD"
+                }
+            )
+        })
+});
 
 
+router.get('/viz/servidoresIntervienen/getRamos', cors(),(req,res)=>{
+    const client = new Client(connectionData);
+    client.connect ();
+
+    client.query(
+        "select ramo from reniresp " +
+        req.filtros ? ( " where "+req.filtros):""+
+        " group by ramo order by ramo "
+    )
+        .then(response => {
+            let rows = response.rows;
+            client.end();
+            return res.status(200).send(
+                {
+                    "status": 200,
+                    "data": rows
+                });
+        })
+        .catch(err => {
+            client.end();
+            console.log("Error : ",err);
+            return res.status(400).send(
+                {
+                    "status" : 400,
+                    "mensaje" : "Error al consultar BD"
+                }
+            )
+        })
+});
+
+router.get('/viz/servidoresIntervienen/getInstituciones', cors(),(req,res)=>{
+    const client = new Client(connectionData);
+    client.connect ();
+
+    client.query(
+        "select institucion from reniresp " +
+        req.filtros? (" where "+req.filtros) : ""+
+        " group by institucion order by institucion "
+    )
+        .then(response => {
+            let rows = response.rows;
+            client.end();
+            return res.status(200).send(
+                {
+                    "status": 200,
+                    "data": rows
+                });
+        })
+        .catch(err => {
+            client.end();
+            console.log("Error : ",err);
+            return res.status(400).send(
+                {
+                    "status" : 400,
+                    "mensaje" : "Error al consultar BD"
+                }
+            )
+        })
+});
+router.get()
 
 module.exports = router;
