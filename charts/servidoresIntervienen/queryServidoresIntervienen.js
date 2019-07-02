275344dbbb9e7577f0b7fef4e63e1278dcdbf5eb
IselaMorquecho <isela.escom@gmail.com>
2019-07-02 18:55:31 -0500

    querys s2

--- charts/servidoresIntervienen/queryServidoresIntervienen.js
+++ charts/servidoresIntervienen/queryServidoresIntervienen.js
@@ -6,16 +6,16 @@ var cors = require('cors');
 const {Client} = require('pg');
 
 const connectionData = {
-    user : process.env.USER_POSTGRES,
-    host : process.env.HOST_POSTGRES,
-    database : process.env.DATABASE_VIZ_S3,
-    password : process.env.PASSWORD_POSTGRES,
-    port : process.env.PORT_POSTGRES
+    user: process.env.USER_POSTGRES,
+    host: process.env.HOST_POSTGRES,
+    database: process.env.DATABASE_VIZ_S3,
+    password: process.env.PASSWORD_POSTGRES,
+    port: process.env.PORT_POSTGRES
 };
 
-router.get('/viz/servidoresIntervienen/getAgrupacionEjercicio', cors(),(req,res)=>{
+router.get('/viz/servidoresIntervienen/getAgrupacionEjercicio', cors(), (req, res) => {
     const client = new Client(connectionData);
-    client.connect ();
+    client.connect();
 
     client.query("select ejercicio, count(*) total  from reniresp group by ejercicio order by ejercicio;")
         .then(response => {
@@ -29,19 +29,19 @@ router.get('/viz/servidoresIntervienen/getAgrupacionEjercicio', cors(),(req,res)
         })
         .catch(err => {
             client.end();
-            console.log("Error : ",err);
+            console.log("Error : ", err);
             return res.status(400).send(
                 {
-                    "status" : 400,
-                    "mensaje" : "Error al consultar BD"
+                    "status": 400,
+                    "mensaje": "Error al consultar BD"
                 }
             )
         })
 });
 
-router.get('/viz/servidoresIntervienen/getAgrupacionPuesto', cors(),(req,res)=>{
+router.get('/viz/servidoresIntervienen/getAgrupacionPuesto', cors(), (req, res) => {
     const client = new Client(connectionData);
-    client.connect ();
+    client.connect();
 
     client.query("select puesto, count(*) total  from reniresp group by puesto order by total desc limit 10;")
         .then(response => {
@@ -55,26 +55,39 @@ router.get('/viz/servidoresIntervienen/getAgrupacionPuesto', cors(),(req,res)=>{
         })
         .catch(err => {
             client.end();
-            console.log("Error : ",err);
+            console.log("Error : ", err);
             return res.status(400).send(
                 {
-                    "status" : 400,
-                    "mensaje" : "Error al consultar BD"
+                    "status": 400,
+                    "mensaje": "Error al consultar BD"
                 }
             )
         })
 });
 
-router.get('/viz/servidoresIntervienen/getAgrupaciones', cors(),(req,res)=>{
+router.post('/viz/servidoresIntervienen/getAgrupaciones', cors(), (req, res) => {
     const client = new Client(connectionData);
-    client.connect ();
+    client.connect();
+
+    let aux = "";
+    if (req.body.filtros) {
+        req.body.filtros.forEach((item, index, array) => {
+            console.log("item: ", item);
+            console.log("index: ", index);
+            aux += (index === 0 ? item : (" and " + item))
+        });
+    }
+
+console.log("Aux: ",aux);
+    let query = "select ejercicio, ramo,institucion, count(*) total  " +
+        " from reniresp " +
+        (req.body.filtros? (" where "+ aux):"")+
+            " group by ejercicio,ramo " + (req.body.grupos ? ("," + req.body.grupos) : "") +
+        " order by total desc ";
+    console.log("load: ", query);
 
     client.query(
-        "select ejercicio, ramo, count(*) total  " + req.campos?req.campos : "" +
-        " from reniresp " +
-        req.filtros ? (" where "+req.filtros) : "" +
-        " group by ejercicio,ramo " + req.grupos ? req.grupos :""+
-          " order by total desc "
+        query
     )
         .then(response => {
             let rows = response.rows;
@@ -87,20 +100,19 @@ router.get('/viz/servidoresIntervienen/getAgrupaciones', cors(),(req,res)=>{
         })
         .catch(err => {
             client.end();
-            console.log("Error : ",err);
+            console.log("Error : ", err);
             return res.status(400).send(
                 {
-                    "status" : 400,
-                    "mensaje" : "Error al consultar BD"
+                    "status": 400,
+                    "mensaje": "Error al consultar BD"
                 }
             )
         })
 });
 
-router.get('/viz/servidoresIntervienen/getEjercicios', cors(),(req,res)=>{
+router.get('/viz/servidoresIntervienen/getEjercicios', cors(), (req, res) => {
     const client = new Client(connectionData);
-    client.connect ();
-
+    client.connect();
     client.query(
         "select ejercicio from reniresp group by ejercicio order by ejercicio "
     )
@@ -115,25 +127,26 @@ router.get('/viz/servidoresIntervienen/getEjercicios', cors(),(req,res)=>{
         })
         .catch(err => {
             client.end();
-            console.log("Error : ",err);
+            console.log("Error : ", err);
             return res.status(400).send(
                 {
-                    "status" : 400,
-                    "mensaje" : "Error al consultar BD"
+                    "status": 400,
+                    "mensaje": "Error al consultar BD"
                 }
             )
         })
 });
 
 
-router.get('/viz/servidoresIntervienen/getRamos', cors(),(req,res)=>{
+router.post('/viz/servidoresIntervienen/getRamos', cors(), (req, res) => {
     const client = new Client(connectionData);
-    client.connect ();
-
+    let query = "select ramo from reniresp " +
+        (req.body.filtros ? (" where " + req.body.filtros) : "") +
+        " group by ramo order by ramo ";
+    console.log("Query: ", query);
+    client.connect();
     client.query(
-        "select ramo from reniresp " +
-        req.filtros ? ( " where "+req.filtros):""+
-        " group by ramo order by ramo "
+        query
     )
         .then(response => {
             let rows = response.rows;
@@ -146,24 +159,25 @@ router.get('/viz/servidoresIntervienen/getRamos', cors(),(req,res)=>{
         })
         .catch(err => {
             client.end();
-            console.log("Error : ",err);
+            console.log("Error : ", err);
             return res.status(400).send(
                 {
-                    "status" : 400,
-                    "mensaje" : "Error al consultar BD"
+                    "status": 400,
+                    "mensaje": "Error al consultar BD"
                 }
             )
         })
 });
 
-router.get('/viz/servidoresIntervienen/getInstituciones', cors(),(req,res)=>{
+router.post('/viz/servidoresIntervienen/getInstituciones', cors(), (req, res) => {
     const client = new Client(connectionData);
-    client.connect ();
-
+    client.connect();
+    let query = "select institucion from reniresp " +
+        (req.body.filtros ? (" where " + req.body.filtros) : "") +
+        " group by institucion order by institucion ";
+    console.log("query: ", query);
     client.query(
-        "select institucion from reniresp " +
-        req.filtros? (" where "+req.filtros) : ""+
-        " group by institucion order by institucion "
+        query
     )
         .then(response => {
             let rows = response.rows;
@@ -176,11 +190,11 @@ router.get('/viz/servidoresIntervienen/getInstituciones', cors(),(req,res)=>{
         })
         .catch(err => {
             client.end();
-            console.log("Error : ",err);
+            console.log("Error : ", err);
             return res.status(400).send(
                 {
-                    "status" : 400,
-                    "mensaje" : "Error al consultar BD"
+                    "status": 400,
+                    "mensaje": "Error al consultar BD"
                 }
             )
         })
